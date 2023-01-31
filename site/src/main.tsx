ebee695f1d5c7000ee7ffcd50e0f334a746f449d
Tim Montesa <me@tmontesa.com>
2023-01-30 19:34:26 -0800

    Added apollo client & hooked into ctx

--- site/src/main.tsx
+++ site/src/main.tsx
@@ -2,9 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+
+const client = new ApolloClient({
+    uri: 'https://flyby-gateway.herokuapp.com/',
+    cache: new InMemoryCache(),
+});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+
 )

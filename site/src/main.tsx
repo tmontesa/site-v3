bf17dde5331de1b24517358a25efa5f2bea59b4b
Tim Montesa <me@tmontesa.com>
2023-02-02 18:29:03 -0800

    Removed default Vite assets & styling

--- site/src/main.tsx
+++ site/src/main.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
 import { ApolloProvider } from '@apollo/client';
 import { getApolloClient } from './apollo';
 
-const CMS_API_URI: string = import.meta.env.VITE_CMS_API_URI
-const CMS_API_TOKEN: string = import.meta.env.VITE_CMS_API_TOKEN
+const CMS_API_URI: string = import.meta.env.VITE_CMS_API_URI;
+const CMS_API_TOKEN: string = import.meta.env.VITE_CMS_API_TOKEN;
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <ApolloProvider client={getApolloClient(CMS_API_URI, CMS_API_TOKEN)}>

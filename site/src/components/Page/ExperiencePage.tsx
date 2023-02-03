c9f8df5d01316dd2dc1e4108a8166c994f78114a
Tim Montesa <me@tmontesa.com>
2023-02-03 12:51:43 -0800

    Added pages & corresponding stylesheets

---
+++ site/src/components/Page/ExperiencePage.tsx
@@ -0,0 +1,12 @@
+import "../../style/Page/Page.scss";
+import "../../style/Page/ExperiencePage.scss";
+
+type Props = {
+    
+}
+
+export function ExperiencePage(props: Props) {
+    return(
+        <div className="page" id="experience-page"></div>
+    )
+}
---
+++ site/src/components/Page/ProjectPage.tsx
@@ -0,0 +1,12 @@
+import "../../style/Page/Page.scss";
+import "../../style/Page/ProjectPage.scss";
+
+type Props = {
+    
+}
+
+export function ProjectPage(props: Props) {
+    return(
+        <div className="page" id="project-page"></div>
+    )
+}

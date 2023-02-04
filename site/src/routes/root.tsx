546481352640a7c8f5ec1ba76d24d39c1d960234
Tim Montesa <me@tmontesa.com>
2023-02-03 18:41:41 -0800

    Added ScrollToTop utility

--- site/src/routes/root.tsx
+++ site/src/routes/root.tsx
@@ -1,10 +1,13 @@
 import { Outlet } from "react-router-dom";
 import { Sidebar } from "../components/Sidebar/Sidebar";
 import "../style/root.scss";
+import ScrollToTop from "../utilities/scrollToTop";
+
 
 export default function Root() {   
     return (
         <div id="root">
+            <ScrollToTop />
             <div id="content">
                 <Sidebar />
                 <Outlet />
